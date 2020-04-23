24bcbf3233cd80aaf0519db156202e262a563599
bkuma <bkuma@DESKTOP-FCLE14V.fios-router.home>
2020-04-23 05:27:42 -0400

    MinnionLaborShifts

---
+++ src/io/bhuwan/MinionLaborShifts.java
@@ -0,0 +1,38 @@
+package io.bhuwan;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+public class MinionLaborShifts {
+
+	public static void main(String[] args) {
+		int [] arr = {1,3,5,3};
+		for(int i:solution(arr, 1)) {
+		System.out.println(i);
+		}
+
+	}
+
+
+	private static int [] solution(int [] data, int n){
+		LinkedHashMap<Integer, Integer> temp= new LinkedHashMap<>();
+		ArrayList<Integer> output = new ArrayList();
+		for(Integer i: data) {
+			if(null == temp.get(i)) {
+			temp.put(i,1);
+			}else {
+				temp.replace(i,temp.get(i)+1);
+			}
+		} 
+
+		for(Integer i: data) {
+			if(null !=temp.get(i))
+			if(temp.get(i) <=n) {
+				output.add(i);
+			}
+		} 
+
+		return output.stream().mapToInt(Integer::intValue).toArray();
+
+	}	
+}
